17d80b8836bf6ac5cb9316c6ce47a42ec2ec90c4
yuxiangchang <yxchang1310@gmail.com>
2025-10-06 21:39:10 +0800

    change project name

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ import { Analytics } from '@vercel/analytics/react'
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "VisionBoard",
+  title: "CloudBoard",
   description: "Modern digital bulletin board system",
 };
 
